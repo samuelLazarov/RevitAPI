0f89bd728a0d3ae69c050130a70fc2f46bb40e08
SamuelLazarov <samuel.lazarov@gmail.com>
2024-01-12 14:19:54 +0200

    RevitPluginExercises

---
+++ MyRevitCommands/MyRevitCommands/Commands/CollectWindows.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class CollectWindows : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+
+            Document doc = uiDoc.Document;
+
+            //Create Filtered Element Collector
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            
+            //Create Filter
+            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Windows);
+
+
+            //Apply Filter
+
+            IList<Element> windows = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+
+            TaskDialog.Show("Windows", string.Format("{0} windows counted!", windows.Count));
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ MyRevitCommands/MyRevitCommands/Commands/GetElementId.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class GetElementId : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //GetUIDocument
+
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //GetDocument
+
+            Document document = uidoc.Document;
+
+            try
+            {
+                //Pick Object
+
+                Reference pickedObject = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                //Retrieve Element
+                ElementId elementId = pickedObject.ElementId;
+
+                Element element = document.GetElement(elementId);
+
+                //Get Element Type
+
+                ElementId eTypeId = element.GetTypeId();
+
+                ElementType eType = document.GetElement(eTypeId) as ElementType;
+
+                //Display Element Id
+
+                if (pickedObject != null)
+                {
+                    TaskDialog.Show("Element Classification", elementId.ToString() + Environment.NewLine
+                        + "Category: " + element.Category.Name + Environment.NewLine
+                        + "Instance: " + element.Name + Environment.NewLine
+                        + "Symbol: " + eType.Name + Environment.NewLine
+                        + "Family: " + eType.FamilyName);
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+             
+            }
+            
+            
+        }
+    }
+}
